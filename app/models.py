e21e2e4a5f00b950389bbca16d531ad011e8ddc1
soash <soash2002@gmail.com>
2024-10-25 15:26:02 +0600

    commit

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import timezone
+
 
 class Team(models.Model):
     name = models.CharField(max_length=20)
